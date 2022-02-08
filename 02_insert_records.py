0153d320ddf4730d24cd407b92f0c19110327d96
anakin1359 <anakin1359@gmail.com>
2022-02-08 22:58:41 +0900

    レコード取得処理定義

---
+++ 02_insert_records.py
@@ -0,0 +1,39 @@
+import os
+
+# from ModelExam.ModelApp.models import Classes
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ModelExam.settings")
+from django import setup
+setup()
+
+from ModelApp.models import Students, Classes
+
+student = Students.objects.get(pk=1)
+for test_result in student.testresults_set.all():
+  print(student.class_fk.name, student.name, test_result.test.name, test_result.score)
+
+from django.db.models import Sum, Avg, Max, Min
+
+# group by (クラス名、テスト名)でデータをグループに分ける
+for class_summary in Classes.objects.values("name", "students__testresults__test__name").annotate(
+    max_score = Max("students__testresults__score"),
+    min_score = Min("students__testresults__score"),
+    avg_score = Avg("students__testresults__score"),
+    sum_score = Sum("students__testresults__score"),
+  ):
+  print(
+    class_summary["name"],
+    class_summary["students__testresults__test__name"],
+    class_summary["max_score"],
+    class_summary["min_score"],
+    class_summary["avg_score"],
+    class_summary["sum_score"],
+  )
+
+# 実際に内部でどのようなQUERYが発行されているかを確認
+print(Classes.objects.values("name", "students__testresults__test__name").annotate(
+    max_score = Max("students__testresults__score"),
+    min_score = Min("students__testresults__score"),
+    avg_score = Avg("students__testresults__score"),
+    sum_score = Sum("students__testresults__score"),
+  ).query)
